7c2894720f5a718c085aa3a6612b22af6bdf796e
3amny <alinochka-yan@mail.ru>
2023-01-04 08:11:29 +0500

    notFoundMiddleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import notFoundMiddleware from './middleware/not-found.js'
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.get('/',(req,res) => {
     res.send('Welcome');
 });
+
+//middleware
+app.use(notFoundMiddleware)
+
+const port = process.env.PORT || 5000;
 
 app.listen(port,() =>{
     console.log(`Server is running on port: ${port}`);
