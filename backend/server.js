ccc8b6158c20ea719a3436ea4154afde88287013
3amny <alinochka-yan@mail.ru>
2023-01-04 09:35:00 +0500

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,31 @@
 import express from "express";
-import notFoundMiddleware from './middleware/not-found.js'
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
 
 const app = express();
+dotenv.config();
 
-app.get('/',(req,res) => {
-    res.send('Welcome');
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
 //middleware
-app.use(notFoundMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,() =>{
-    console.log(`Server is running on port: ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
