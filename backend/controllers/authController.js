fe324f9348320b8ddfe0a3d10f8e32c82a8b2766
3amny <alinochka-yan@mail.ru>
2023-01-09 17:12:27 +0500

    signup handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,16 +4,17 @@ import {BadRequest} from '../error/index.js'
 
 
 const register = async (req,res ) => {
-    const {firstName, lastName, email, password} = req.body
-    if(!firstName || !lastName || !email || !password){
+    const {firstName, lastName, email, password, phone} = req.body
+    if(!firstName || !lastName || !email || !password || !phone){
         throw new BadRequest('Please provide all information')
     }
     const emailIsInUse = await User.findOne({email})
     if(emailIsInUse){
         throw new BadRequest('Email is already in use')
     }
-       const user = await User.create(req.body)
-       res.status(StatusCodes.CREATED).json({user})
+       const user = await User.create({firstName, lastName, email, password, phone})
+      const token = user.createJWT()
+       res.status(StatusCodes.CREATED).json({user:{email: user.email, firstName: user.firstName, lastName: user.lastName, phone: user.phone, city: user.city, address: user.address}, token, city: user.city, address: user.address})
 }
 
 const login = async (req,res) => {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from 'bcryptjs'
+import jwt  from "jsonwebtoken";
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -28,6 +30,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide your password"],
     minlength: 7,
+    select: false,
   },
   phone: {
     type: String,
@@ -48,6 +51,15 @@ const UserSchema = new mongoose.Schema({
     default: 'Address'
   },
 });
+// hashing passwords
 
+UserSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({userId:this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
+}
 
 export default mongoose.model('User', UserSchema);
